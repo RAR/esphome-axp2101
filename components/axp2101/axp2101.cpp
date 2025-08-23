b8f1755c034f560a34ca94dbe61e53acddce056b
Andrew Rankin <andrew@eiknet.com>
2025-08-23 10:40:35 -0400

    i2c int pin

--- components/axp2101/axp2101.cpp
+++ components/axp2101/axp2101.cpp
@@ -253,13 +253,13 @@ void AXP2101Component::setup()
     gpio_config_t io_conf = {};
     io_conf.intr_type = GPIO_INTR_NEGEDGE; 
     io_conf.mode = GPIO_MODE_INPUT;       
-    io_conf.pin_bit_mask = (1ULL << GPIO_NUM_35); 
+    io_conf.pin_bit_mask = (1ULL << GPIO_NUM_21); 
     io_conf.pull_up_en = GPIO_PULLUP_ENABLE;     
     io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE; 
     gpio_config(&io_conf);                       
 
     gpio_install_isr_service(0);
-    gpio_isr_handler_add(GPIO_NUM_35, setFlag, (void*) GPIO_NUM_35);
+    gpio_isr_handler_add(GPIO_NUM_21, setFlag, (void*) GPIO_NUM_21);
 
     // Disable all interrupts
     PMU.disableIRQ(XPOWERS_AXP2101_ALL_IRQ);
