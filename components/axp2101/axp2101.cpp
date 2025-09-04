1a1a2d1b8345efa9eee403e32b11c7d904ba0f01
Andrew Rankin <andrew@eiknet.com>
2025-09-04 14:07:14 -0400

    bit more dealing with masks

--- components/axp2101/axp2101.cpp
+++ components/axp2101/axp2101.cpp
@@ -1,6 +1,7 @@
 #include "axp2101.h"
 #include "esphome/core/log.h"
 #include <unistd.h>
+#include "driver/gpio.h"
 
 // for Core2 v1.1 (AXP2101)
 // ALDO2 == LCD+TOUCH RST
@@ -36,24 +37,25 @@ void AXP2101Component::setup() {
     ESP_LOGD(TAG, "Running Setup:");
     ESP_LOGD(TAG, " General Setup...");
     this->i2c_write_register8_array(reg_data_axp2101_first);
-    ESP_LOGD(TAG, " Display Reset...");
-    this->i2c_write_register8_array(reg_data_axp2101_reset);
-    ESP_LOGD(TAG, " Enabling Display...");
+    //ESP_LOGD(TAG, " Running Reset...");
+    //this->i2c_write_register8_array(reg_data_axp2101_reset);
     this->i2c_write_register8_array(reg_data_axp2101_second);
-    this->setBrightness(200);
+
+    this->setBrightness(232);
 }
 
 void AXP2101Component::dump_config() {}
-float AXP2101Component::get_setup_priority() const { return setup_priority::DATA; }
+float AXP2101Component::get_setup_priority() const { return setup_priority::BUS; }
 void AXP2101Component::update() {
     ESP_LOGD(TAG, "Running Update...");
 }
 
 void AXP2101Component::setBrightness(uint8_t brightness) {
   // BLDO1
+  brightness = ((brightness + 641) >> 5);
   ESP_LOGD(TAG, "Setting Brightness to %i", brightness);
+
   if (brightness) {
-    brightness = ((brightness + 641) >> 5);
     this->bitOn(0x90, 0x10); // BLDO1 enable reg, bit
   }
   else {
@@ -71,12 +73,20 @@ void AXP2101Component::bitOff(uint8_t reg, uint8_t bit) {
 }
 
 void AXP2101Component::writeRegister8(uint8_t reg, uint8_t data, uint8_t mask) {
-  auto res = this->write_bytes(reg, &data, 1);
+  uint8_t tmp[1] = {};
+  ESP_LOGD(TAG, "Setting reg: %X, data: %X, mask: %X", reg, data, mask);
+  if (mask) {
+    auto resread = this->read_register(reg, tmp, 1);
+    uint8_t newdata[1] = { (tmp[0] & ~mask) | (data & mask) };
+    auto reswrite = this->write_register(reg, newdata, 1);
+  }
+  else {
+    auto reswrite = this->write_register(reg, &data, 1);
+  }
 }
 
 void AXP2101Component::i2c_write_register8_array(const uint8_t* reg_data_mask) {
   while (reg_data_mask[0] != 0xFF || reg_data_mask[1] != 0xFF || reg_data_mask[2] != 0xFF) {
-    ESP_LOGD(TAG, "    Setting reg: %X, data: %X, mask: %X", reg_data_mask[0], reg_data_mask[1], reg_data_mask[2]);
     this->writeRegister8(reg_data_mask[0], reg_data_mask[1], reg_data_mask[2]);
     reg_data_mask += 3;
     usleep(3);
